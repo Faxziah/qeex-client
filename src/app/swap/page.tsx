64732fed12cbf8e9845bc2be9f17aaa594241c8e
Sasun Avakian <avakian.s@boostra.ru>
2025-03-24 05:24:08 +0300

    Added connect button component

---
+++ src/app/swap/page.tsx
@@ -0,0 +1,11 @@
+import ConnectButton from "@/app/swap/components/ConnectButton";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <main>
+        <ConnectButton/>
+      </main>
+    </div>
+  );
+}
